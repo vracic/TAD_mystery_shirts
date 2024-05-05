c3a92c8dd1fac3fdbdab94841c6ded6753782dbe
vracic <vracictin@gmail.com>
2024-05-05 16:21:15 +0200

    fortify

--- mystery/routes/web.php
+++ mystery/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('auth.dashboard');
+})->middleware(['auth', 'verified']);
+
+
